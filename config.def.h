d72ba23593621c032921f944d528e705febe52c1
Jayanth Parthsarathy <jparthaa@gmail.com>
2023-09-17 19:50:20 +0530

    Change alignment in config.def.h

--- config.def.h
+++ config.def.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
     /* function      format                                                                               argument */
-    {run_command,    "     %s                                                                       ",   "music_status"},
+    {run_command,    "     %s                                                                       ",  "music_status"},
     {run_command,    " %s ",                                                                              "volume"},
     {run_command,    " %s ",                                                                              "battery"},
     {run_command,    " %s ",                                                                              "wifi"},
